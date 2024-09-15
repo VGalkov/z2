fcee9ff414ed6f05e1642fce02edc2cf6fbccf4a
v.galkov <v.galkov@argustelecom.ru>
2024-09-15 15:48:29 +0400

    initial system

--- src/main/java/ru/example/Something.java
+++ src/main/java/ru/example/Something.java
@@ -5,9 +5,13 @@ public class Something {
     private final int intVar1;
     private final int intVar2;
 
+    private final Something.Chaild Chaild;
+
+
     public Something(int intVar1, int intVar2) {
         this.intVar1 = intVar1;
         this.intVar2 = intVar2;
+        this.Chaild = new Chaild(result1());
     }
 
 
@@ -19,4 +23,30 @@ public class Something {
     public int result2() {
         return intVar1 * intVar2;
     }
+
+    public int getIntVar1() {
+        return intVar1;
+    }
+
+    public int getIntVar2() {
+        return intVar2;
+    }
+
+    public Something.Chaild getChaild() {
+        return Chaild;
+    }
+
+    private class Chaild {
+        private final int i;
+
+
+        public Chaild(int i) {
+            this.i = i;
+        }
+
+        public int getI() {
+            return i;
+        }
+    }
+
 }
--- src/test/java/ru/example/AppTest.java
+++ src/test/java/ru/example/AppTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-
+import java.util.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
@@ -13,16 +15,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 @ExtendWith(MockitoExtension.class)
 public class AppTest {
-
+    @Mock
+    List<Integer> mockedList;
     private static Something somethingTestObject1;
     private static Something somethingTestObject2;
     private static Something somethingTestObject3;
 
     @BeforeAll //@BeforeEach
     public static void init() {
-        somethingTestObject1 = new Something(4,5);
-        somethingTestObject2 = new Something(4,0);
-        somethingTestObject3 = new Something(4,-5);
+        somethingTestObject1 = new Something(4, 5);
+        somethingTestObject2 = new Something(4, 0);
+        somethingTestObject3 = new Something(4, -5);
     }
 
     @Test
@@ -37,6 +40,14 @@ public class AppTest {
         assertEquals(20, somethingTestObject1.result2());
         assertEquals(0, somethingTestObject2.result2());
         assertEquals(-20, somethingTestObject3.result2());
+    }
+    @Test
+    public void testResult3() {
+        mockedList.add(4);
+        Mockito.when(mockedList.get(0)).thenReturn(4);
+
+        assertEquals(4, mockedList.get(0));
+        assertEquals(80, somethingTestObject1.result2() * mockedList.get(0));
     }
 
     @AfterAll
